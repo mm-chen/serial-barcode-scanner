7a4c7a9107af18bd0542b371bff5c01012956556
Johannes Rudolph <johannes.rudolph@gmx.com>
2019-10-05 17:22:14 +0200

    Extend Database for automatic Orders

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,14 +1,118 @@
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY NOT NULL CHECK (id < 10000000000000 and (10 - (((id / 1000000000000 % 10) + (id / 100000000000 % 10) * 3 + (id / 10000000000 % 10) + (id / 1000000000 % 10) * 3 + (id / 100000000 % 10) + (id / 10000000 % 10) * 3 + (id / 1000000 % 10) + (id / 100000 % 10) * 3 + (id / 10000 % 10) + (id / 1000 % 10) * 3 + (id / 100 % 10) + (id / 10 % 10) * 3) % 10)) % 10 == (id / 1 % 10)), name TEXT, amount INTEGER NOT NULL DEFAULT 0, category INTEGER REFERENCES categories, deprecated BOOLEAN NOT NULL DEFAULT 0);
-CREATE TABLE IF NOT EXISTS sales (user INTEGER NOT NULL REFERENCES users, product INTEGER NOT NULL REFERENCES products, timestamp INTEGER NOT NULL DEFAULT 0);
-CREATE TABLE IF NOT EXISTS restock (user INTEGER NOT NULL REFERENCES users, product INTEGER NOT NULL REFERENCES products, amount INTEGER NOT NULL DEFAULT 0, timestamp INTEGER NOT NULL DEFAULT 0, price INTEGER NOT NULL DEFAULT 0, supplier INTEGER, best_before_date INTEGER);
-CREATE TABLE IF NOT EXISTS prices (product INTEGER NOT NULL REFERENCES products, valid_from INTEGER NOT NULL DEFAULT 0, memberprice INTEGER NOT NULL DEFAULT 0, guestprice INTEGER NOT NULL DEFAULT 0);
-CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY NOT NULL, email TEXT, firstname TEXT NOT NULL, lastname TEXT NOT NULL, gender TEXT, street TEXT, plz INTEGER, city TEXT, pgp TEXT, disabled BOOLEAN NOT NULL DEFAULT 0, hidden BOOLEAN NOT NULL DEFAULT 0, sound_theme TEXT, language TEXT, joined_at INTEGER NOT NULL DEFAULT 0);
-CREATE TABLE IF NOT EXISTS authentication(user INTEGER PRIMARY KEY NOT NULL REFERENCES users, password TEXT, pin TEXT, session CHARACTER(20), superuser BOOLEAN NOT NULL DEFAULT 0, auth_users BOOLEAN NOT NULL DEFAULT 0, auth_products BOOLEAN NOT NULL DEFAULT 0, auth_cashbox BOOLEAN NOT NULL DEFAULT 0, disabled BOOLEAN NOT NULL DEFAULT 0);
-CREATE TABLE IF NOT EXISTS supplier(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, city TEXT, postal_code TEXT, street TEXT, phone TEXT, website TEXT);
-CREATE TABLE IF NOT EXISTS cashbox_diff(id INTEGER PRIMARY KEY AUTOINCREMENT, user INTEGER NOT NULL REFERENCES users, amount INTEGER NOT NULL, timestamp INTEGER NOT NULL DEFAULT 0);
-CREATE TABLE IF NOT EXISTS ean_aliases (id INTEGER PRIMARY KEY NOT NULL, real_ean INTEGER NOT NULL REFERENCES products);
-CREATE TABLE IF NOT EXISTS categories (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT);
-CREATE TABLE IF NOT EXISTS rfid_users (rfid TEXT, user INTEGER NOT NULL REFERENCES users);
+CREATE TABLE IF NOT EXISTS products (
+  id INTEGER PRIMARY KEY NOT NULL CHECK (id < 10000000000000 and (10 - (((id / 1000000000000 % 10) + (id / 100000000000 % 10) * 3 + (id / 10000000000 % 10) + (id / 1000000000 % 10) * 3 + (id / 100000000 % 10) + (id / 10000000 % 10) * 3 + (id / 1000000 % 10) + (id / 100000 % 10) * 3 + (id / 10000 % 10) + (id / 1000 % 10) * 3 + (id / 100 % 10) + (id / 10 % 10) * 3) % 10)) % 10 == (id / 1 % 10)),
+  name TEXT,
+  amount INTEGER NOT NULL DEFAULT 0,
+  category INTEGER REFERENCES categories,
+  deprecated BOOLEAN NOT NULL DEFAULT 0,
+  max INTEGER NOT NULL DEFAULT 1,
+  threshold INTEGER NOT NULL DEFAULT 0,
+  notify INTEGER NOT NULL DEFAULT 0,
+  vpe INTEGER NOT NULL DEFAULT 1,
+  supplier INTEGER REFERENCES supplier,
+  supplier_number TEXT
+);
+
+CREATE TABLE IF NOT EXISTS sales (
+  user INTEGER NOT NULL REFERENCES users,
+  product INTEGER NOT NULL REFERENCES products,
+  timestamp INTEGER NOT NULL DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS restock_new (
+  user INTEGER NOT NULL REFERENCES users,
+  product INTEGER NOT NULL REFERENCES products,
+  amount INTEGER NOT NULL DEFAULT 0,
+  timestamp INTEGER NOT NULL DEFAULT 0,
+  price INTEGER NOT NULL DEFAULT 0,
+  supplier INTEGER REFERENCES supplier,
+  best_before_date INTEGER,
+  order_id INTEGER REFERENCES orders
+);
+
+CREATE TABLE IF NOT EXISTS prices (
+  product INTEGER NOT NULL REFERENCES products,
+  valid_from INTEGER NOT NULL DEFAULT 0,
+  memberprice INTEGER NOT NULL DEFAULT 0,
+  guestprice INTEGER NOT NULL DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY NOT NULL,
+  email TEXT,
+  firstname TEXT NOT NULL,
+  lastname TEXT NOT NULL,
+  gender TEXT,
+  street TEXT,
+  plz INTEGER,
+  city TEXT,
+  pgp TEXT,
+  disabled BOOLEAN NOT NULL DEFAULT 0,
+  hidden BOOLEAN NOT NULL DEFAULT 0,
+  sound_theme TEXT,
+  language TEXT,
+  joined_at INTEGER NOT NULL DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS authentication(
+  user INTEGER PRIMARY KEY NOT NULL REFERENCES users,
+  password TEXT,
+  pin TEXT,
+  session CHARACTER(20),
+  superuser BOOLEAN NOT NULL DEFAULT 0,
+  auth_users BOOLEAN NOT NULL DEFAULT 0,
+  auth_products BOOLEAN NOT NULL DEFAULT 0,
+  auth_cashbox BOOLEAN NOT NULL DEFAULT 0,
+  disabled BOOLEAN NOT NULL DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS supplier_new (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  city TEXT,
+  postal_code TEXT,
+  street TEXT,
+  phone TEXT,
+  website TEXT,
+  supplier_number TEXT,
+  customer_number TEXT,
+  order_format TEXT,
+  order_transport TEXT
+);
+
+CREATE TABLE IF NOT EXISTS cashbox_diff(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user INTEGER NOT NULL REFERENCES users,
+  amount INTEGER NOT NULL,
+  timestamp INTEGER NOT NULL DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS ean_aliases (
+  id INTEGER PRIMARY KEY NOT NULL,
+  real_ean INTEGER NOT NULL REFERENCES products
+);
+
+CREATE TABLE IF NOT EXISTS categories (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+);
+
+CREATE TABLE IF NOT EXISTS rfid_users (
+  rfid TEXT,
+  user INTEGER NOT NULL REFERENCES users
+);
+
+CREATE TABLE IF NOT EXISTS orders
+(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  product INTEGER NOT NULL REFERENCES products,
+  amount INTEGER NOT NULL,
+  created_at INTEGER NOT NULL,
+  orderd_at INTEGER,
+  received_at INTEGER,
+  supplier INTEGER NOT NULL REFERENCES supplier
+);
+
 CREATE INDEX IF NOT EXISTS invoiceindex ON sales (user ASC, timestamp DESC);
+
 COMMIT;
--- sql/views.sql
+++ sql/views.sql
@@ -1,135 +1,285 @@
-BEGIN TRANSACTION;
-CREATE VIEW IF NOT EXISTS stock AS SELECT id, name, category, amount FROM products WHERE deprecated = 0 OR amount != 0;
-CREATE VIEW IF NOT EXISTS purchaseprices AS SELECT product, SUM(price * amount) / SUM(amount) AS price FROM restock GROUP BY product;
+BEGIN
+   TRANSACTION;
+CREATE VIEW salesView AS
+SELECT
+   timestamp,
+   products.id AS productid,
+   name AS productname,
+   user AS userId,
+   users.firstname,
+   users.lastname,
+   CASE
+      WHEN
+         user < 0
+      THEN
+(
+         SELECT
+            SUM(price * amount) / SUM(amount)
+         FROM
+            restock
+         WHERE
+            restock.product = products.id
+            AND restock.timestamp <= sales.timestamp )
+         ELSE
+(
+            SELECT
+               CASE
+                  WHEN
+                     user = 0
+                  THEN
+                     guestprice
+                  ELSE
+                     memberprice
+               END
+            FROM
+               prices
+            WHERE
+               product = products.id
+               AND valid_from <= timestamp
+            ORDER BY
+               valid_from DESC LIMIT 1 )
+   END
+   AS price
+            FROM
+               sales
+               INNER JOIN
+                  products
+                  ON sales.product = products.id
+               LEFT JOIN
+                  users
+                  ON sales.user = users.id
+            WHERE
+               users.id >= 0
+            ORDER BY
+               timestamp desc;
+CREATE VIEW IF NOT EXISTS stock AS
+SELECT
+   id,
+   name,
+   category,
+   amount
+FROM
+   products
+WHERE
+   deprecated = 0
+   OR amount != 0;
+CREATE VIEW IF NOT EXISTS purchaseprices AS
+SELECT
+   product,
+   SUM(price * amount) / SUM(amount) AS price
+FROM
+   restock
+GROUP BY
+   product;
 CREATE VIEW IF NOT EXISTS invoice AS
-	SELECT user, timestamp, id AS productid, name AS productname,
-		CASE
-			WHEN user < 0 THEN
-				(SELECT SUM(price * amount) / SUM(amount)
-					FROM restock
-					WHERE restock.product = id AND restock.timestamp <= sales.timestamp
-				)
-			else
-				(SELECT
-					CASE
-						WHEN user=0 THEN guestprice
-						else memberprice
-					END
-					FROM prices
-					WHERE product = id AND valid_from <= timestamp
-					ORDER BY valid_from DESC LIMIT 1)
-			END AS price
-		FROM sales INNER JOIN products ON sales.product = products.id
-		ORDER BY timestamp;
+SELECT
+   user,
+   timestamp,
+   id AS productid,
+   name AS productname,
+   CASE
+      WHEN
+         user < 0
+      THEN
+(
+         SELECT
+            SUM(price * amount) / SUM(amount)
+         FROM
+            restock
+         WHERE
+            restock.product = id
+            AND restock.timestamp <= sales.timestamp )
+         else
+(
+            SELECT
+               CASE
+                  WHEN
+                     user = 0
+                  THEN
+                     guestprice
+                  else
+                     memberprice
+               END
+            FROM
+               prices
+            WHERE
+               product = id
+               AND valid_from <= timestamp
+            ORDER BY
+               valid_from DESC LIMIT 1)
+   END
+   AS price
+            FROM
+               sales
+               INNER JOIN
+                  products
+                  ON sales.product = products.id
+            ORDER BY
+               timestamp;
 CREATE VIEW IF NOT EXISTS current_cashbox_status AS
-	SELECT (
-		(
-			SELECT SUM(
-				(
-					SELECT guestprice
-						FROM prices
-						WHERE product = s.product AND valid_from <= s.timestamp
-						ORDER BY valid_from DESC LIMIT 1
-				)
-			) FROM sales s WHERE user = 0
-		)
-		+
-		(
-			SELECT SUM(amount) FROM cashbox_diff
-		)
-	) AS amount;
-CREATE VIEW IF NOT EXISTS singleDaysSinceFirstSale AS
-	WITH RECURSIVE
-	singledays(x) AS (
-	 SELECT (SELECT strftime('%s',datetime( (select s.timestamp from sales s where s.user >= 0 order by s.timestamp asc limit 1), 'unixepoch','start of year'))*1)
-	 UNION ALL
-	 SELECT x+86400 FROM singledays
-	  LIMIT 10000
-	)
-	SELECT x as start, (x+86399) as end FROM singledays where x < strftime('%s','now')*1.0;
+SELECT
+( (
+   SELECT
+      SUM( (
+      SELECT
+         guestprice
+      FROM
+         prices
+      WHERE
+         product = s.product
+         AND valid_from <= s.timestamp
+      ORDER BY
+         valid_from DESC LIMIT 1 ) )
+      FROM
+         sales s
+      WHERE
+         user = 0 ) + (
+         SELECT
+            SUM(amount)
+         FROM
+            cashbox_diff ) ) AS amount;
+CREATE VIEW IF NOT EXISTS singleDaysSinceFirstSale AS WITH RECURSIVE singledays(x) AS
+(
+   SELECT
+(
+      SELECT
+         strftime('%s', datetime( (
+         select
+            s.timestamp
+         from
+            sales s
+         where
+            s.user >= 0
+         order by
+            s.timestamp asc limit 1), 'unixepoch', 'start of year'))*1)
+         UNION ALL
+         SELECT
+            x + 86400
+         FROM
+            singledays LIMIT 10000
+)
+SELECT
+   x as start,
+   (
+      x + 86399
+   )
+   as
+end
+FROM
+   singledays
+where
+   x < strftime('%s', 'now')*1.0;
 CREATE VIEW IF NOT EXISTS statistic_productsperday AS
-	SELECT
-		date(sdsp.start, 'unixepoch') day,
-		( SELECT count(*) from salesView s1 where s1.timestamp >= sdsp.start and s1.timestamp <= sdsp.end and p.id = s1.productId) as numOfProducts,
-		( select sum(s2.price) from salesView s2 where s2.timestamp >= sdsp.start and s2.timestamp <= sdsp.end and p.id = s2.productId) as total,
-		p.name product,
-		p.id productId,
-		c.name category
-	FROM 
-		 singleDaysSinceFirstSale sdsp, products p
-		left join categories as c on (c.id = p.category)
-	ORDER BY 
-		 day desc, 
-		 category asc, 
-		 product asc;
+SELECT
+   date(sdsp.start, 'unixepoch') day,
+   (
+      SELECT
+         count(*)
+      from
+         salesView s1
+      where
+         s1.timestamp >= sdsp.start
+         and s1.timestamp <= sdsp.
+         end
+         and p.id = s1.productId
+   )
+   as numOfProducts,
+   (
+      select
+         sum(s2.price)
+      from
+         salesView s2
+      where
+         s2.timestamp >= sdsp.start
+         and s2.timestamp <= sdsp.
+         end
+         and p.id = s2.productId
+   )
+   as total,
+   p.name product,
+   p.id productId,
+   c.name category
+FROM
+   singleDaysSinceFirstSale sdsp,
+   products p
+   left join
+      categories as c
+      on (c.id = p.category)
+ORDER BY
+   day desc,
+   category asc,
+   product asc;
 CREATE VIEW IF NOT EXISTS statistic_productspermonth AS
-	SELECT
-		strftime('%m',day) month,
-		strftime('%Y',day) year,
-		sum(numOfProducts) numOfProducts,
-		sum(total) total ,
-		product,
-		productId,
-		category
-	FROM
-		statistic_productsperday
-	GROUP BY
-		product,month,year
-	ORDER BY 
-		 year desc, 
-		 month desc,
-		 category asc, 
-		 product asc;
+SELECT
+   strftime('%m', day) month,
+   strftime('%Y', day) year,
+   sum(numOfProducts) numOfProducts,
+   sum(total) total,
+   product,
+   productId,
+   category
+FROM
+   statistic_productsperday
+GROUP BY
+   product,
+   month,
+   year
+ORDER BY
+   year desc,
+   month desc,
+   category asc,
+   product asc;
 CREATE VIEW IF NOT EXISTS statistic_productsperyear AS
-	SELECT
-		year,
-		sum(numOfProducts) numOfProducts,
-		sum(total) total ,
-		product,
-		productId,
-		category
-	FROM 
-		statistic_productspermonth
-	GROUP BY 
-		 product,
-		 year
-	ORDER BY
-		year desc,
-		category asc, 
-		product asc;
+SELECT
+   year,
+   sum(numOfProducts) numOfProducts,
+   sum(total) total,
+   product,
+   productId,
+   category
+FROM
+   statistic_productspermonth
+GROUP BY
+   product,
+   year
+ORDER BY
+   year desc,
+   category asc,
+   product asc;
 CREATE VIEW IF NOT EXISTS statistic_salesperday AS
-	SELECT
-		day,
-		sum(numOfProducts) numOfProducts,
-		sum(total) total
-	FROM
-		statistic_productsperday
-	GROUP BY 
-		day	
-	ORDER BY
-		day desc;
+SELECT
+   day,
+   sum(numOfProducts) numOfProducts,
+   sum(total) total
+FROM
+   statistic_productsperday
+GROUP BY
+   day
+ORDER BY
+   day desc;
 CREATE VIEW IF NOT EXISTS statistic_salespermonth AS
-	SELECT
-		strftime('%m',day) month,
-		strftime('%Y',day) year,
-		sum(numOfProducts) numOfProducts,
-		sum(total) total
-	FROM
-		statistic_salesperday
-	GROUP BY
-		month,year
-	ORDER BY
-		year desc,
-		month desc;
+SELECT
+   strftime('%m', day) month,
+   strftime('%Y', day) year,
+   sum(numOfProducts) numOfProducts,
+   sum(total) total
+FROM
+   statistic_salesperday
+GROUP BY
+   month,
+   year
+ORDER BY
+   year desc,
+   month desc;
 CREATE VIEW IF NOT EXISTS statistic_salesperyear AS
-	SELECT
-		year,
-		sum(numOfProducts) numOfProducts,
-		sum(total) total
-	FROM
-		statistic_salespermonth
-	GROUP BY
-		year
-	ORDER BY
-		year desc;
+SELECT
+   year,
+   sum(numOfProducts) numOfProducts,
+   sum(total) total
+FROM
+   statistic_salespermonth
+GROUP BY
+   year
+ORDER BY
+   year desc;
 COMMIT;
