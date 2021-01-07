295dcd43f1d6f0b84c6fee890e84b8f17e77ab5d
Malte Modler <github@malte-modler.de>
2021-01-07 19:57:24 +0100

    now you can use Space-API for display-on-off

--- src/display-on-off/display-on-off.sh
+++ src/display-on-off/display-on-off.sh
@@ -1,27 +1,54 @@
 #!/bin/bash
 
-function getMqttConfig
+function getConfig
 {
-	echo `mdbus2 -s io.mainframe.shopsystem.Config /io/mainframe/shopsystem/config io.mainframe.shopsystem.Config.GetString MQTT $1 | cut -d"'" -f 2`
+	VAR=$(echo `busctl --system call io.mainframe.shopsystem.Config /io/mainframe/shopsystem/config io.mainframe.shopsystem.Config GetString ss $1 $2 | sed -s 's;s ;;'`)
+	VAR=${VAR//\"}
+	echo $VAR
 }
 
-BROKER=$(getMqttConfig broker)
-TOPIC=$(getMqttConfig displayOnOffTopic)
-ON=$(getMqttConfig displayOn)
-OFF=$(getMqttConfig displayOff)
+SOURCE="$(getConfig DISPLAY use)"
 
+if [ "$SOURCE" = "mqtt" ]; then
+	BROKER=$(getConfig MQTT broker)
+	TOPIC=$(getConfig MQTT topic)
+	ON=$(getConfig MQTT displayOn)
+	OFF=$(getConfig MQTT displayOff)
 
-mosquitto_sub -h $BROKER -t $TOPIC | while read RAW_DATA
-do
-	case $RAW_DATA	in
-	$ON)
-		vbetool dpms on
-		;;
-	$OFF)
-		vbetool dpms off
-		;;
-	*)
-		#vbetool dpms on
-		;;
-	esac
-done
+	mosquitto_sub -h "$BROKER" -t "$TOPIC" | while read RAW_DATA
+	do
+		case $RAW_DATA	in
+		$ON)
+			vbetool dpms on
+			;;
+		$OFF)
+			vbetool dpms off
+			;;
+		*)
+			#vbetool dpms on
+			;;
+		esac
+	done
+elif [ "$SOURCE" = "spaceapi" ]; then
+	URL=$(getConfig SPACEAPI url)
+	POLLRATE=$(getConfig SPACEAPI pollrate)
+	while : ; do
+		JSON=$(wget -q -O - $URL) || break
+		case $(echo $JSON | jq '.state.open') in
+		"true")
+                        vbetool dpms on
+			;;
+                "false")
+                        vbetool dpms off
+			;;
+                *)
+                        #vbetool dpms on
+                        ;;
+                esac
+
+		sleep $POLLRATE
+	done
+else
+	exit 1
+fi
+
